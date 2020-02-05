bb6c5a38ffbf2fe1335a7c1157d7891e5e92892f
Marko Vujanic <crashxx@gmail.com>
2020-02-05 14:41:56 +0100

    Add 2 layout column styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import {GenericStyles, AppWrapper, MainWrapper} from 'src/styles'
 import {Sidebar, Main} from 'src/styles/layout'
+import styled from 'styled-components'
+
+const ANIMALS = [
+  'barnyard',
+  'bird',
+  'cat',
+  'dog',
+  'horse',
+  'rabbit',
+  'scales-fins-other',
+  'small-furry',
+]
 
 function App() {
   return (
@@ -8,11 +20,63 @@ function App() {
       <GenericStyles />
 
       <MainWrapper>
-        <Sidebar>Side</Sidebar>
-        <Main>Main </Main>
+        <Sidebar>
+          <Form
+            method="POST"
+            onSubmit={e => {
+              e.preventDefault()
+              console.log('hi')
+            }}
+          >
+            <label>
+              Location
+              <input type="text" />
+            </label>
+            <label>
+              Animal
+              <select>
+                {ANIMALS.map(a => (
+                  <option key={a}>{a}</option>
+                ))}
+              </select>
+            </label>
+            <label>
+              Breed
+              <select>
+                <option></option>
+              </select>
+            </label>
+            <button>Submit</button>
+          </Form>
+        </Sidebar>
+        <Main>
+          <Content>
+            <Pets pets={{}} />
+          </Content>
+        </Main>
       </MainWrapper>
     </AppWrapper>
   )
 }
 
+function Pets() {
+  return <Pet />
+}
+
+function Pet() {
+  return <article>Hello I am Pet</article>
+}
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+
+  select,
+  input {
+    display: block;
+    width: 100%;
+  }
+`
+const Content = styled.div``
 export default App
--- src/styles/layout.js
+++ src/styles/layout.js
@@ -1,15 +1,25 @@
 import styled from 'styled-components'
-import {screens} from 'src/styles/settings'
+import {screens, colors} from 'src/styles/settings'
 
 export const Sidebar = styled.div`
   background-color: #fff;
+  border-radius: 5px;
   flex: 0 1 300px;
-
+  align-self: start;
+  padding: 2rem;
   @media (min-width: ${screens.md}) {
     margin-right: 2rem;
   }
+
+  label {
+    margin-bottom: 2rem;
+  }
 `
+
 export const Main = styled.div`
-  background-color: #000;
+  background-color: ${colors.indigo[500]}
+  border-radius: 5px;
+  padding: 2rem;
+  align-self: start;
   flex: 1;
 `
